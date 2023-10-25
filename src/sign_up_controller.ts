ac03de663075e1e239ab4afb6f7f75049799c7b2
glesiosantos <glesioss@gmail.com>
2023-10-25 12:12:16 -0300

    feat: ensure SignUpController return an error when passwordConfirmation is not provided

--- src/sign_up_controller.ts
+++ src/sign_up_controller.ts
@@ -1,6 +1,6 @@
 export class SignUpController {
   async handle(httpRequest: any): Promise<any> {
-    const requiredField = ['name', 'email', 'password']
+    const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredField) {
       if (!httpRequest.body[field]) {
