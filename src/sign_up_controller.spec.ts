fd601a3e3f5e58825778199ec6b36b6053a8fd68
glesiosantos <glesioss@gmail.com>
2023-10-26 08:20:09 -0300

    test: ensure SignUpController return 400 when password confirmation is fails

--- src/sign_up_controller.spec.ts
+++ src/sign_up_controller.spec.ts
@@ -56,4 +56,19 @@ describe('Name of the group', () => {
     expect(response.statusCode).toBe(400)
     expect(response.body).toEqual(new Error('Missing param: passwordConfirmation'))
   })
+
+  it('should return 400 when password confirmation is fails', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const response = await sut.handle(httpRequest)
+    expect(response.statusCode).toBe(400)
+    expect(response.body).toEqual(new Error('Invalid param: passwordConfirmation'))
+  })
 })
