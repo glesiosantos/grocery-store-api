1d139e00382c776520ee36d43ddf5ed96d8f9b08
glesiosantos <glesioss@gmail.com>
2023-11-03 09:54:43 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/criptography/bcrypt_adapter.spec.ts
+++ src/infra/criptography/bcrypt_adapter.spec.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import { BCryptAdapater } from './bcrypt_adapter'
+import { Encrypter } from '../../data/protocols/encrypter'
 
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> { return new Promise(resolve => { resolve('any_hash') }) }
@@ -7,23 +8,25 @@ jest.mock('bcrypt', () => ({
 
 const salt: number = 12
 
+const makeSut = (): Encrypter => new BCryptAdapater(salt)
+
 describe('BCrypter Adapter', () => {
   it('should calls bcrypt with correct values', async () => {
-    const sut = new BCryptAdapater(salt)
+    const sut = makeSut()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should throws when bcrypt throws', async () => {
-    const sut = new BCryptAdapater(salt)
+    const sut = makeSut()
     jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
     const promise = sut.encrypt('any_value')
     await expect(promise).rejects.toThrow()
   })
 
   it('should return hash on success', async () => {
-    const sut = new BCryptAdapater(salt)
+    const sut = makeSut()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('any_hash')
   })
