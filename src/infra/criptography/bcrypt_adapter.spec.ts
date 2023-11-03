8373db90c7eddce5b0a1bbdb25ddaf79f1a0d050
glesiosantos <glesioss@gmail.com>
2023-11-03 09:52:07 -0300

    test: ensure BCryptAdapter thrown when bcrypt throws

--- src/infra/criptography/bcrypt_adapter.spec.ts
+++ src/infra/criptography/bcrypt_adapter.spec.ts
@@ -8,11 +8,18 @@ jest.mock('bcrypt', () => ({
 const salt: number = 12
 
 describe('BCrypter Adapter', () => {
-  it('should calls BCrypt with correct values', async () => {
+  it('should calls bcrypt with correct values', async () => {
     const sut = new BCryptAdapater(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  it('should throws when bcrypt throws', async () => {
+    const sut = new BCryptAdapater(salt)
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
   })
 
   it('should return hash on success', async () => {
